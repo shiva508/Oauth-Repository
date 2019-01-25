fbba2270d78d2d5b619ee88c2fa8ab2151766559
shiva <shiva@pgi-amarendrad1.Prodigy.local>
2019-01-25 17:13:31 +0530

    Satish

--- src/main/java/com/devglan/config/TYest.java
+++ src/main/java/com/devglan/config/TYest.java
@@ -8,6 +8,7 @@ public class TYest {
 public static void main(String[] args) {
 	PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	System.out.println(passwordEncoder.encode("shiva"));
-	
+	System.out.println(System.getProperty("user.name"));
+	System.out.println(System.getenv("USERNAME"));
 }
 }
